24efc5f46f2d1d6ce592a0452987694f941e1517
Numan Khalil <numankhalil27@gmail.com>
2024-03-24 13:19:08 +0500

    Lecture 19 changes

--- src/pages/Contact.jsx
+++ src/pages/Contact.jsx
@@ -1,9 +1,18 @@
 import React from "react"
+import { Button, Container } from "react-bootstrap";
 
 const Contact = () => {
     return (
         <div>
             <h1>Welcome to Contact</h1>
+
+            <Container>
+                <Button>
+                    <h3>
+                        Lorem ipsum dolor sit, amet consectetur adipisicing elit. Sequi, minus!
+                    </h3>
+                </Button>
+            </Container>
         </div>
     )
 }
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -3,8 +3,8 @@ import React from "react"
 const Home = () => {
     return (
         <div>
-
             <h1>Welcome to Home</h1>
+            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Aliquam vitae consequatur, quod praesentium quidem maiores, fugit dignissimos eius natus porro dolor alias laboriosam. Ipsa hic vero delectus maxime et odio!</p>
         </div>
     )
 }
